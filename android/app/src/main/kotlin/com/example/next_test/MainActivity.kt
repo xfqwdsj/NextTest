47aa8d46e789f0f3790fb64f64e2809b21fe1b8a
xfqwdsj <xfqwdsj@qq.com>
2022-07-23 12:40:35 +0800

    ci: 更新 CI 构建 apk

--- android/app/src/main/kotlin/com/example/next_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.next_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/xyz/xfqlittlefan/nexttest/MainActivity.kt
@@ -0,0 +1,5 @@
+package xyz.xfqlittlefan.nexttest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
